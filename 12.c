06ceab935b26b2bdcfa0fcd8ca6c1d1407961014
13863049525 <w42023@outlook.com>
2019-07-06 19:12:17 +0800

    new file:   12.c

---
+++ 12.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+
+char an[]="检查环境",a1[]="抽取游戏核心",a2[]="获取秘钥",a3[]="解码",a4[]="consulting",a5[]="授权检验",a6[]="检验失败";
+int Sleep(int  cn){
+int i,i1,ck=cn*100;
+for(i1=0;i1<ck;i1++){
+for(i=0;i<6000;i++);}
+}
+int  prtc(char pk[],int am){
+	int  i;
+printf("%s\n",pk);
+printf("[");
+for(i=0;i<am;i=i+1){
+	printf(">");
+	Sleep(100);}
+	printf("]OK\n");
+	printf("---------------\n");
+	
+	
+	
+	
+	
+	
+}
+int  main(){
+	printf("    本程序为技术交流所用,使用必须同意相关条约\n");
+	printf("    请输入y以确认:");
+	getchar() ;
+prtc(an,5);
+
+prtc(a1,10); 
+prtc(a2,13);
+prtc(a3,15);
+prtc(a4,10); 
+prtc(a5,13); 
+printf("检验失败!\n请检查授权情况");
+ 
+	
+}
+
+
+
+
+
+
+
+
+
